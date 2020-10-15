ac8d8ac9e11f7c6ca176bd59037f57204ecd5c14
Ronaldo <ronaldogdn@gmail.com>
2020-10-14 21:01:48 -0300

    adicionado link entre / e /records

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
-
+import './styles.css';
 const Records = () => (
-    <h1>Records</h1>
+    <div className="page-container">
+        <table className="records-table" cellPadding="0" cellSpacing="0">
+            <thead>
+                <tr>
+                    <th>INSTANTE</th>
+                    <th>NOME</th>
+                    <th>IDADE</th>
+                    <th>PLATAFORMA</th>
+                    <th>GÊNERO</th>
+                    <th>TÍTULO DO GAME</th>                    
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>13/10/2020</td>
+                    <td>Ronaldo</td>
+                    <td>33</td>
+                    <td>XBOX</td>
+                    <td>Ação</td>
+                    <td>minority report</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
 );
 
